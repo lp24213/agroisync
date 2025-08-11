8e80b765d33ca724b8cd43e84c27afe641b47138
Luís Paulo Oliveira <luispaulo-de-oliveira@hotmail.com>
2025-08-10 22:49:02 -0300

    HOSTING CORRIGIDO - Configuração completa S3AndCloudFront

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,11 +16,5 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
-  },
-  "hosting": {
-    "S3AndCloudFront": {
-      "S3BucketName": "string",
-      "WebsiteURL": "string"
-    }
   }
 }
