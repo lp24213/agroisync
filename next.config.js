ddf4eee32af85ff2d874407d0b10ba01b02b30c6
Luís Paulo Oliveira <luispaulo-de-oliveira@hotmail.com>
2025-08-21 15:08:23 -0300

    FIX: Configuração estática para AWS Amplify - diretório out correto

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
   reactStrictMode: false,
   swcMinify: false,
   eslint: {
     ignoreDuringBuilds: true
+  },
+  images: {
+    unoptimized: true
   }
 }
 
